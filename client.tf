78b9b3bae6a6bbc6fbf10e8051b970ed23476971
Justin Clayton <claytjus@amazon.com>
2019-01-10 11:24:12 -0800

    add egress rule

--- client.tf
+++ client.tf
@@ -89,6 +89,13 @@ resource "aws_security_group" "neptune_client" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_iam_instance_profile" "profile" {
